cb27a57e6effe5b34a8af4af82748eb86407b29a
adjavon <adjavon@users.noreply.github.com>
2016-11-03 17:26:30 +0100

    CA: skeleton parse, generate, revoke, error
    Added parse, generate, revoke, and error functions. 
    Parse returns the processing function which needs to be run, and the variables it needs (simplified for now).
    Generate, revoke, and error currently print and return a generic response.
    The generic response is sent back to client.

--- ca.py
+++ ca.py
@@ -5,7 +5,61 @@
 import socket
 import sys
 
- 
+def error():
+    '''
+	Good old error function. Returns an error response to send to the client
+    '''
+    print 'Error: received an invalid request'
+    return 'Not a valid request. Usage: FLAG Username [Options]';
+
+def generate(uname, options): # options will be : name, email, company = 'iMovies', address= '' ):
+    '''
+	This function generates a certificate with the given information. And returns what to send back 
+    '''
+    print 'Generated a certificate for ', uname
+    response = 'Hello ' + uname + ", we have generated a certificate for you with options: " + options + '!'
+    return response;
+
+def revoke (uname, reason=''):
+    '''
+	This function revokes a certificate with the given information, and returns what to send back
+    '''
+    print 'Revoked certificate of user %s' %uname
+    response = 'Hello ' + uname +', we have revoked your certificate, with for reason: ' + reason
+    return response;
+
+#defining some useful functions TODO: put these in classes or modules or threads or something
+def parse (data):
+    '''
+	For now, this function parses data (a string) and returns a string that is the response
+	This will later be the function that returns whether we should run Generate or Revoke and with what information, or return a parse error 
+    '''
+    
+    flag = ''
+    request = {'uname':'', 'options':''}
+    accepted_flags = {'G', 'R'}
+    #parse data to pick a function
+    #Data of the form: FLAG Username Options 
+    #FLAG is G for generate and R for revoke; Options can be however many (and anything for now)
+    parts = data.split(' ', 2)
+    if len(parts) >=3:
+        flag = parts[0]
+        request['uname'] = parts[1]
+        request['options'] = parts[2]
+    print "Received: %s" %data, "... parsing"
+    #check that the required data (FLAG and Username") has been provided 
+    if ( flag not in accepted_flags) | (request['uname']==''):
+            def process(request):
+		return error();
+    else :
+        if flag=='G':
+	    def process(request):
+		return generate(request['uname'], request['options']);
+        else :
+	    def process(request):
+		return revoke(request['uname'], request['options']);
+    return process, request;
+
 HOST = 'localhost'   # Symbolic name, meaning all available interfaces
 PORT = 8888 # Arbitrary non-privileged port
 server_address = (HOST, PORT)
@@ -27,7 +81,7 @@ s.listen(10)
 print 'Socket now listening'
 k=0
 #now keep talking with the client
-while k<1:
+while k<1: #how many connections do we want to accept? while 1 for infinite loop
     #wait to accept a connection - blocking call
     conn, client_address = s.accept()
     print 'Connected with ' + client_address[0] + ':' + str(client_address[1])
@@ -35,8 +89,13 @@ while k<1:
     #receive and return data, blocking call to recv
     buf = 1024
     data = conn.recv(buf)
-    print "Received: %s" %data
-    conn.send("We have received your data. Thank you. ")     
+    #parse data to pick a function
+    #Data of the form: FLAG Username Options 
+    #FLAG is G for generate and R for revoke; Options can be however many (and anything for now)
+    process, request = parse(data)
+    response = process(request);
+    print 'Parsed. Reponding: ', response
+    conn.sendall(response)     
     print "Closing connection with client ", client_address
     conn.close
     k+=1
