a3298fb2b39634396b7c3da890421455d2bb2032
adjavon <adjavon@gmail.com>
2016-11-24 03:25:14 +0100

    backup fix

--- safeguard.sh
+++ safeguard.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find /home/guest -path '/home/guest/backup/*' -type f -exec chmod 600 {} \;
-find /home/guest -path '/home/guest/backup/*' -type f -exec chown root:root {} \;
+find /home/guest -path '/home/guest/backup/*' -exec chmod 600 {} \;
+find /home/guest -path '/home/guest/backup/*' -exec chown root:root {} \;
 
