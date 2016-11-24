2f70beff8d55866e81cd8debafcc4571d4409df0
Rami Khalil <thesavagekoder@gmail.com>
2016-11-24 02:51:37 +0100

    Safeguard BS

--- safeguard.sh
+++ safeguard.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find . -path 'backup/*' -type f -exec chmod 600 {} \;
-find . -path 'backup/*' -type f -exec chown root:root {} \;
+find /home/guest -path '/home/guest/backup/*' -type f -exec chmod 600 {} \;
+find /home/guest -path '/home/guest/backup/*' -type f -exec chown root:root {} \;
 
